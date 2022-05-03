ee7baa2c4ddae9ee8f8b5922916b3b98a50097a7
Thai Pangsakulyanont <dtinth@spacet.me>
2022-05-03 13:57:08 +0000

    Other repo

--- sync.sh
+++ sync.sh
@@ -4,5 +4,6 @@ export GIT_COMMITTER_NAME="creatorsgarten[bot]"
 export GIT_AUTHOR_NAME="creatorsgarten[bot]"
 export GIT_COMMITTER_EMAIL="creatorsgarten[bot]@users.noreply.github.com"
 export GIT_AUTHOR_EMAIL="creatorsgarten[bot]@users.noreply.github.com"
+cd stack
 git add --all && git commit -m "Update stack" || echo "Nothing to commit"
 git push
