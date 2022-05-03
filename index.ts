94f5254e9cf0b5af431401d3ebdc150a9e3bed72
dtinth on MBP M1 <dtinth@spacet.me>
2022-05-03 21:07:25 +0700

    refactor, new members, remove stack.json from config repo

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import * as github from "@pulumi/github";
+import * as github from '@pulumi/github'
 
-new github.TeamMembership("membership-for-heypoom", {
-  teamId: "6011674",
-  username: "heypoom",
-});
-new github.TeamMembership("membership-for-pankace", {
-  teamId: "6011674",
-  username: "pankace",
-});
+const creator = (username: string) =>
+  new github.TeamMembership(`membership-for-${username}`, {
+    teamId: '6011674',
+    username,
+  })
+
+creator('betich')
+creator('dtinth')
+creator('heypoom')
+creator('pankace')
